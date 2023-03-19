c89de1a6ad38c8a571b1e7f60ad774df210fe1af
RocioAlday <roo.alday@gmail.com>
2023-03-19 13:27:49 -0300

    cajaDePandoraFtionModified

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if (Number.isInteger(numero)){
+        if (numero%2===0){
+            return cambioBinario(numero)
+        }else return numero.toString(16);
     }
+    }
+
+
+
+function cambioBinario(num) {   
+let binario= '';                    
+while (num!==0){                    
+binario+= num%2;                  
+num= Math.floor(num/2);          
+}
+return binario.split('').reverse().join('');  
+}
+
