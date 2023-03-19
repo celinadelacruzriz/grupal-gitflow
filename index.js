0086af61d88f72e0d70046fcf97986423d9e4714
Javier <52515632+javlucky@users.noreply.github.com>
2023-03-19 13:40:03 -0300

    new function object

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function convertNumber(num) {
     return num % 2 === 0 ? num.toString(2) : num.toString(16);
   }
+
+  function javier() {
+    return {
+      name: "Javier",
+      age: 39,
+      nationality: "Argentino"
+    }
+  }
